a6403866bc99133aea36fe319ad5feacf4b53563
jpiv <jjpizzo4@gmail.com>
2015-03-31 12:03:04 -0700

    make Will Smith's head rotate

--- src/faceDancer.js
+++ src/faceDancer.js
@@ -2,10 +2,21 @@ var FaceDancer = function(top, left, timeBetweenSteps){
   BlinkyDancer.apply(this, arguments);
   this.$node = $('<img class="dancer death" src="face.png" width="150px"/>');
   this.timeBetweenSteps = timeBetweenSteps;
-  this.setPosition(top, left);console.log($(".death"));
+  this.setPosition(top, left);
+  this.oldTop = top;
   var selfish = this.selfishMode.bind(this);
-  $(".death").on('click', function(){
+  this.$node.on('click', function(){
     selfish();
+  });
+  $('.death').on('mouseover', function(){
+    console.log('here!');
+    // debugger;
+    $(this).css('transform', 'rotate(14deg)');
+  });
+  $('.death').on('mouseleave', function(){
+    console.log('here!');
+    // debugger;
+    $(this).css('transform', 'rotate(0deg)');
   });
 };
 
@@ -14,19 +25,21 @@ FaceDancer.prototype.constructor = FaceDancer;
 FaceDancer.prototype.oldStep = Dancer.prototype.step;
 
 FaceDancer.prototype.selfishMode = function(){
-  var top = Number(this.$node.css('top').slice(0,7));
-  var left = Number(this.$node.css('left').slice(0,7));
-  var closestNode, nodeTop, nodeLeft, closest = 100000, distance;
+  // debugger;
   for(var i = 0; i < window.dancers.length; i++){
-    nodeTop = Number(window.dancers[i].$node.css('top').slice(0,7));
-    nodeLeft = Number(window.dancers[i].$node.css('left').slice(0,7));
-    distance = Math.sqrt(Math.pow(nodeLeft-left, 2) + Math.pow(nodeTop-top, 2));
-    if(distance < closest){
-      closestNode = window.dancers[i].$node;
-      closest = distance;
+    window.dancers[i].setPosition(window.dancers[i].oldTop, 0);
+    // if(window.dancers[i]['timeBetweenSteps'] === this['timeBetweenSteps'])
     }
-  }
-  closestNode.css("left", 0);
+    var top = $("body").height()/2;
+    var left = $("body").width()/2;
+      this.setPosition(top, left);
 };
 
 
+    // nodeTop = Number(window.dancers[i].$node.css('top').slice(0,7));
+    // nodeLeft = Number(window.dancers[i].$node.css('left').slice(0,7));
+    // distance = Math.sqrt(Math.pow(nodeLeft-left, 2) + Math.pow(nodeTop-top, 2));
+    // if(distance < closest){
+    //   closestNode = window.dancers[i].$node;
+    //   closest = distance;
+    // }
